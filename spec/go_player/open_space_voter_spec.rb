21ba664978bfc49046c258ac6d457bc469da5f24
William Dix <william.dix@getbraintree.com>
2012-11-27 19:47:14 -0600

    fix test errors

--- spec/go_player/open_space_voter_spec.rb
+++ spec/go_player/open_space_voter_spec.rb
@@ -2,9 +2,13 @@ require_relative '../spec_helper'
 require_relative '../../lib/go_player/open_space_voter'
 require_relative '../../lib/go_player/board_recorder'
 require_relative '../../lib/go_player/position'
+require_relative '../../lib/go_player/move'
+require_relative '../../lib/go_player/colors'
 
 describe GoPlayer::OpenSpaceVoter do
-  let(:played){ [ GoPlayer::Position.new(19, 'A') ] }
+  let(:played){
+    [ GoPlayer::Move.new(GoPlayer::Colors::Black, GoPlayer::Position.new(19, 'A')) ]
+  }
 
   it "vetoes if board recorder reports that space has a stone" do
     recorder = GoPlayer::BoardRecorder.new(played)
@@ -15,6 +19,6 @@ describe GoPlayer::OpenSpaceVoter do
   it "does not vote if space does not have a stone" do
     recorder = GoPlayer::BoardRecorder.new(played)
     voter = GoPlayer::OpenSpaceVoter.new(recorder)
-    voter.vote(GoPlayer::Position.new(19, 'A')).should be_nil
+    voter.vote(GoPlayer::Position.new(17, 'A')).should be_nil
   end
 end
